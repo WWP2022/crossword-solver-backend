16c3189c2774dda623973ac2c33936906fbabb3a
Przemysław Wątroba <przemyslaw.watroba@gmail.com>
2022-11-25 01:20:16 +0100

    Add logging to app

--- app/clients/minio_client.py
+++ app/clients/minio_client.py
@@ -1,5 +1,9 @@
 from minio import Minio
 
+from docker_logs import get_logger
+
+logger = get_logger('minio_client')
+
 HOST = "localhost:9000"
 ACCESS_KEY = "admin"
 SECRET_KEY = "admin123"
@@ -22,3 +26,4 @@ def put_processed_image(user_id, file):
     file_name = file.rsplit('/', 1)[-1]  # get only crossword_name
     path = user_id + "/" + file_name
     minio_client.fput_object(BUCKET_NAME, path, file)
+    logger.info(f'save processed image for client: {user_id}')
