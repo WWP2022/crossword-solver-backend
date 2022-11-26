1d04c6131544cb8594f6cfe2e8646c9faaf7d2d6
Przemysław Wątroba <przemyslaw.watroba@gmail.com>
2022-11-27 00:36:31 +0100

    Add deploy to the server

--- app/clients/minio_client.py
+++ app/clients/minio_client.py
@@ -1,18 +1,23 @@
+import os
 from minio import Minio
 
-from docker_logs import get_logger
+from app.utils.docker_logs import get_logger
 
 logger = get_logger('minio_client')
 
-HOST = "localhost:9000"
-ACCESS_KEY = "admin"
-SECRET_KEY = "admin123"
+
+HOST = os.environ.get("MINIO_HOST", "localhost")
+PORT = os.environ.get("MINIO_PORT", "9000")
+HOST_WITH_PORT = HOST + ":" + PORT
+
+ACCESS_KEY = os.environ.get("MINIO_ROOT_USER", "admin")
+SECRET_KEY = os.environ.get("MINIO_ROOT_PASSWORD", "admin123")
 SECURE = False
 
 BUCKET_NAME = "processed-images"
 
 minio_client = Minio(
-    HOST,
+    HOST_WITH_PORT,
     access_key=ACCESS_KEY,
     secret_key=SECRET_KEY,
     secure=SECURE
---
+++ app/clients/postgres_client.py
@@ -0,0 +1,23 @@
+import os
+import psycopg2
+
+from app.utils.docker_logs import get_logger
+
+logger = get_logger('postgres_client')
+
+HOST = os.environ.get("POSTGRES_HOST", "localhost")
+DATABASE = os.environ.get("POSTGRES_DB", "crossword-solver_dev")
+USER = os.environ.get("POSTGRES_USER", "postgres")
+PASSWORD = os.environ.get("POSTGRES_PASSWORD", "postgres")
+
+conn = psycopg2.connect(
+    host=HOST,
+    database=DATABASE,
+    user=USER,
+    password=PASSWORD)
+
+cursor = conn.cursor()
+
+
+def esa():
+    print(cursor.name)
